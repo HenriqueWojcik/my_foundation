d294988e6f25d2210cd341cabee86756cec209e3
Henrique Wojcik <henriquewojcik@brq.com>
2023-12-04 16:11:37 -0300

    test files

---
+++ test/repositories/base_repository_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_foundation/main.dart';
+
+class BaseRepositoryTest extends BaseRepository {}
+
+void main() {
+  late BaseRepositoryTest sut;
+
+  setUp(() {
+    sut = BaseRepositoryTest();
+  });
+
+  test('return right when is success on the task', () async {
+    final (Failure? failure, int? data) = await sut.doAsync<int>(() async => 1);
+
+    expect(data, 1);
+    expect(failure, null);
+  });
+
+  test(
+    'return left when has erros on the task',
+    () async {
+      final Exception exception = Exception('Error');
+
+      final (Failure? failure, int? data) =
+          await sut.doAsync<int>(() async => throw exception);
+
+      expect(data, null);
+      expect(failure?.message, exception.toString());
+    },
+  );
+}
---
+++ test/src/state/app_state_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_foundation/main.dart';
+
+void main() {
+  late AppState sut;
+
+  setUp(() => sut = AppState());
+
+  test(
+      'when call onLoad method, should loading set true and value, erros set as null',
+      () async {
+    sut.onLoad();
+
+    expect(sut.loading, true);
+    expect(sut.data, null);
+    expect(sut.error, null);
+  });
+
+  test(
+      'when call set value, should have data and loading set false and error as null',
+      () async {
+    const int value = 1;
+    sut.value = value;
+
+    expect(sut.data, value);
+    expect(sut.loading, false);
+    expect(sut.error, null);
+  });
+
+  test(
+      'when call set error, should have error and loading set false and data as null',
+      () async {
+    final Failure failure = Failure(message: '', title: '');
+    sut.fail = failure;
+
+    expect(sut.error, failure);
+    expect(sut.data, null);
+    expect(sut.loading, false);
+  });
+
+  test('should initialize app state corretly', () {
+    const int value = 1;
+    sut = AppState.init(value);
+
+    expect(sut.data, value);
+    expect(sut.loading, false);
+    expect(sut.error, null);
+  });
+}
