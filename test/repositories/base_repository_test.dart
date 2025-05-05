8c2077e3ab4128fbefe26092e94c42579d14b0cb
Henrique Wojcik <henriquewojcik@brq.com>
2025-05-05 16:49:27 -0300

    refactoring: rename to foundation.dart

--- test/repositories/base_repository_test.dart
+++ test/repositories/base_repository_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_foundation/main.dart';
+import 'package:my_foundation/foundation.dart';
 
 class BaseRepositoryTest extends BaseRepository {}
 
